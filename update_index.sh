7101ef4ae10b6d510740e8855f3bd7e60f9f7edb
slavoo <4722492+slavoo@users.noreply.github.com>
2020-02-08 22:09:38 +0000

    added parse-server chart

--- update_index.sh
+++ update_index.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-helm package src/* 
+if [[ $# == 0 ]];then 
+  echo please provide name of the chart to repackage;
+  exit 1;
+fi
+
+if [[ ! -e src/$1 ]];then
+  echo failed to find package with name $1. Check the src folder.
+  exit 1;
+fi
+
+helm package src/$1
 helm repo index --url https://slavoo.github.io/helm-charts/ .
+
+echo all done. You can now commit and push.
